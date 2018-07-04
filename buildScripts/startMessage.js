86507130f97c5536fa61381b477f4d62d0d17889
RK Meiappan <radhakrishnan.meiappan@wolterskluwer.com>
2018-07-04 12:30:36 +0530

    added babel for transpiling

---
+++ buildScripts/startMessage.js
@@ -0,0 +1,6 @@
+
+//var chalk = require("chalk"); //common js for node - ES5 code
+
+import chalk from "chalk";  //Module syntax - Standard implemented in ES6. This requires transpiling by babel to ES5
+
+console.log(chalk.green('Starting app in dev mode...'));
