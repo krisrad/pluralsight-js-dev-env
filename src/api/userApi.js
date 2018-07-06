2f9f9fb11132a705dc8542b3d68106a6cfb617b7
RK Meiappan <radhakrishnan.meiappan@wolterskluwer.com>
2018-07-06 13:47:03 +0530

    added json mock api and made the application to use this mock api data to display on the web page

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -1,11 +1,26 @@
 import "whatwg-fetch";
+import getBaseUrl from "./baseUrl";
+
+const baseUrl = getBaseUrl();
 
 export function getUsers() {
   return get('users');
 }
 
+export function deleteUser(id) {
+  return del(`users/${id}`);
+}
+
 function get(url) {
-  return fetch(url).then(onSuccess, onError);
+  console.log("baseurl = " + baseUrl);
+  return fetch(baseUrl + url).then(onSuccess, onError);
+}
+
+function del(url) {
+  const request = new Request(baseUrl + url, {
+    method: "DELETE"
+  });
+  return fetch(request).then(onSuccess, onError);
 }
 
 function onSuccess(response) {
